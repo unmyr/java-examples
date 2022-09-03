5c750ac4dc5b5f4ce957ed8c318ee22153d35846
unmyr <56982541+unmyr@users.noreply.github.com>
2022-09-03 19:48:11 +0900

    feat(net): Add an example of SpringBoot.

---
+++ maven-projects/net/greeter-using-spring-boot/src/main/java/com/example/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/hello/{name}")
+	public Greeting greeting(@PathVariable(value = "name") String name) {
+		return new Greeting(String.format(template, name));
+	}
+}
