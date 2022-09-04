9bf159ff0868773b8bd0fb81221e9c4bbd8340ef
unmyr <56982541+unmyr@users.noreply.github.com>
2022-09-04 12:26:01 +0900

    refactor(greeter-using-spring-boot): Remove unused local variables.

--- maven-projects/net/greeter-using-spring-boot/src/main/java/com/example/GreetingController.java
+++ maven-projects/net/greeter-using-spring-boot/src/main/java/com/example/GreetingController.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello/{name}")
 	public Greeting greeting(@PathVariable(value = "name") String name) {
