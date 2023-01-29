bdefacc71ec4410821ec613460fc8e999f85f108
unmyr <56982541+unmyr@users.noreply.github.com>
2023-01-30 02:14:21 +0900

    refactor: Update Apache HTTP Client 4.5.14 to 5.2.1.

--- maven-projects/net/Apache-HttpClient/get-json/src/main/java/com/example/App.java
+++ maven-projects/net/Apache-HttpClient/get-json/src/main/java/com/example/App.java
@@ -1,10 +1,10 @@
 package com.example;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Hello world!
@@ -14,8 +14,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        HttpClient client = HttpClientBuilder.create().build();
-        try {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
 
             HttpGet getRequest = new HttpGet(
                 "https://fakerestapi.azurewebsites.net/api/v1/Authors"
@@ -23,16 +22,16 @@ public class App
 
             // Set the API media type in http accept header
             getRequest.addHeader("Accept", "application/json");
-            HttpResponse httpResponse = client.execute(getRequest);
-
-            ObjectMapper mapper = new ObjectMapper();
-            FakeRestApiAuthors[] authors = mapper.readValue(
-                httpResponse.getEntity().getContent(),
-                FakeRestApiAuthors[].class
-            );
-
-            for (FakeRestApiAuthors author : authors) {
-                System.out.println(author.toString());
+            try (CloseableHttpResponse httpResponse = client.execute(getRequest)) {
+                ObjectMapper mapper = new ObjectMapper();
+                FakeRestApiAuthors[] authors = mapper.readValue(
+                    httpResponse.getEntity().getContent(),
+                    FakeRestApiAuthors[].class
+                );
+    
+                for (FakeRestApiAuthors author : authors) {
+                    System.out.println(author.toString());
+                }                    
             }
         } catch (java.net.MalformedURLException e) {
             e.printStackTrace();
