287c9949cf91717010f097a02b33a0b28c7e7152
unmyr <56982541+unmyr@users.noreply.github.com>
2023-01-30 03:12:48 +0900

    refactor: Replaced the implementation to use the response handler.

--- maven-projects/net/Apache-HttpClient/get-json/src/main/java/com/example/App.java
+++ maven-projects/net/Apache-HttpClient/get-json/src/main/java/com/example/App.java
@@ -1,8 +1,12 @@
 package com.example;
+import java.io.InputStream;
+
+import org.apache.hc.client5.http.ClientProtocolException;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.message.StatusLine;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -22,16 +26,29 @@ public class App
 
             // Set the API media type in http accept header
             getRequest.addHeader("Accept", "application/json");
-            try (CloseableHttpResponse httpResponse = client.execute(getRequest)) {
-                ObjectMapper mapper = new ObjectMapper();
-                FakeRestApiAuthors[] authors = mapper.readValue(
-                    httpResponse.getEntity().getContent(),
-                    FakeRestApiAuthors[].class
-                );
-    
-                for (FakeRestApiAuthors author : authors) {
-                    System.out.println(author.toString());
-                }                    
+
+            FakeRestApiAuthors[] authors = client.execute(getRequest, response -> {
+                if (response.getCode() >= 300) {
+                    throw new ClientProtocolException(new StatusLine(response).toString());
+                }
+
+                final HttpEntity responseEntity = response.getEntity();
+                if (responseEntity == null) {
+                    return null;
+                }
+
+                try (InputStream inputStream = responseEntity.getContent()) {
+                    ObjectMapper mapper = new ObjectMapper();
+                    return mapper.readValue(
+                        inputStream,
+                        FakeRestApiAuthors[].class
+                    );
+                }
+            });
+            client.close();
+
+            for (FakeRestApiAuthors author : authors) {
+                System.out.println(author.toString());
             }
         } catch (java.net.MalformedURLException e) {
             e.printStackTrace();
