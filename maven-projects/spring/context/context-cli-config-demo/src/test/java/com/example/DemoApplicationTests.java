596291403accd63a1b7fea833eddebfb6b5a4f7f
unmyr <56982541+unmyr@users.noreply.github.com>
2023-02-05 13:21:27 +0900

    test(context-cli-config-demo): Add tests.

--- maven-projects/spring/context/context-cli-config-demo/src/test/java/com/example/DemoApplicationTests.java
+++ maven-projects/spring/context/context-cli-config-demo/src/test/java/com/example/DemoApplicationTests.java
@@ -1,25 +1,31 @@
 package com.example;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @ExtendWith(OutputCaptureExtension.class)
 class DemoApplicationTests {
 	@Test
-	void contextLoads() {
-	}
-
-	@Test
 	public void testRun(CapturedOutput output) throws Exception {
-		DemoApplication app = new DemoApplication();
-		app.run(new String[] {"John"});
-		Assertions.assertTrue(
-			output.toString().contains("Hello John"), 
-			"output does not contain John"
-		);
+		try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+			DemoApplicationTests.class
+		)) {
+			ctx.addApplicationListener(new ContextObserver());
+
+			DemoApplication app = new DemoApplication();
+			ctx.start();
+			app.run(new String[] {"John"});
+			ctx.stop();
+			ctx.close();
+
+			Assertions.assertThat(output).contains("ContextStartedEvent");
+			Assertions.assertThat(output).contains("Hello John");
+			Assertions.assertThat(output).contains("context is closed.");
+		}
 	}
 
 }
